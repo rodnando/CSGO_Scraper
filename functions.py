c8ef590272ca0062fabd9a5d69a2de725bc555a3
Fernando Rodrigues <nandooh.rodrigues@hotmail.com>
2023-09-03 19:58:27 -0300

    add getHTML function

---
+++ functions.py
@@ -0,0 +1,27 @@
+from urllib.request import Request, urlopen
+import urllib.request
+import re
+
+
+def getHTML(url):
+    # Open the URL
+    # Spoof the user agent
+
+    header = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
+    "X-Requested-With": "XMLHttpRequest"
+    }
+    
+    request = Request(url, headers=header)
+    
+    # Read the response as HTML
+    try:
+        urlopen(request).read()
+        html = urlopen(request).read().decode('ascii', 'ignore')
+        if len(re.findall('error-desc', html)) > 0:
+            return None
+        else:
+            return html
+    except urllib.error.HTTPError as err:
+        print("%s for %s" % (err.code, url))
+        return None
