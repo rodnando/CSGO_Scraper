eed875884a1339be9dbc9dffd8a71cd8ed81941e
Fernando Rodrigues <nandooh.rodrigues@hotmail.com>
2023-09-03 20:13:49 -0300

    add coments about getMatch function

--- functions.py
+++ functions.py
@@ -29,10 +29,13 @@ def getHTML(url):
 def getMatchID(last):
     html = getHTML("https://www.hltv.org/results?offset=0")
 
+    # Find where the match information are
     matchIDs = re.findall('"(.*?000"><a href="/matches/.*?)"', html)
     
+    # Delete unnecessary data 
     matchIDs = [matchIDs[i].split('/', 2)[-1] for i in range(0, len(matchIDs))]
 
+    # Split in two columns: Id (contains the unique match identifier) and Tittle
     matchIDs = [[matchIDs[i].split('/')[0], matchIDs[i]] for i in range(0, len(matchIDs))]
 
     return matchIDs
