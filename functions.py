09bbf29946f1a386287cd6a58800d0b40cea3456
Fernando Rodrigues <nandooh.rodrigues@hotmail.com>
2023-09-25 11:25:08 -0300

    Create helper.py to help in the data processing

--- functions.py
+++ functions.py
@@ -1,33 +1,11 @@
-from urllib.request import Request, urlopen
-import urllib.request
 import re
+from helper import *
 
+    
+def getMatchID(offset):
+    # Get matches information
 
-def getHTML(url):
-    # Open the URL
-    # Spoof the user agent
-
-    header = {
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
-    "X-Requested-With": "XMLHttpRequest"
-    }
-    
-    request = Request(url, headers=header)
-    
-    # Read the response as HTML
-    try:
-        urlopen(request).read()
-        html = urlopen(request).read().decode('ascii', 'ignore')
-        if len(re.findall('error-desc', html)) > 0:
-            return None
-        else:
-            return html
-    except urllib.error.HTTPError as err:
-        print("%s for %s" % (err.code, url))
-        return None
-    
-def getMatchID(last):
-    html = getHTML("https://www.hltv.org/results?offset=0")
+    html = getHTML("https://www.hltv.org/results?offset={}").format(offset)
 
     # Find where the match information are
     matchIDs = re.findall('"(.*?000"><a href="/matches/.*?)"', html)
@@ -39,3 +17,4 @@ def getMatchID(last):
     matchIDs = [[matchIDs[i].split('/')[0], matchIDs[i]] for i in range(0, len(matchIDs))]
 
     return matchIDs
+
---
+++ helper.py
@@ -0,0 +1,34 @@
+from multiprocessing.dummy import Pool as ThreadPool
+from urllib.request import Request, urlopen
+import urllib.request
+from html import getHTML
+import csv
+import sys
+
+def getHTML(url):
+    # Open the URL
+    # Spoof the user agent
+
+    header = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
+    "X-Requested-With": "XMLHttpRequest"
+    }
+    
+    request = Request(url, headers=header)
+    
+    # Read the response as HTML
+    try:
+        urlopen(request).read()
+        html = urlopen(request).read().decode('ascii', 'ignore')
+        if len(re.findall('error-desc', html)) > 0:
+            return None
+        else:
+            return html
+    except urllib.error.HTTPError as err:
+        print("{} for {}".format(err.code, url))
+        return None
+
+def endCheck(matchIDs, stop):
+    if stop in matchIDs:
+        return False
+    return True
