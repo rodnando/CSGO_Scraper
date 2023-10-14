a88ec357ac0630fe6eee8ad1573a97fc8646bc73
Fernando Rodrigues <nandooh.rodrigues@hotmail.com>
2023-10-14 16:50:22 -0300

    add function to get map overview in each match

--- functions.py
+++ functions.py
@@ -3,9 +3,7 @@ from helper import *
 import pandas as pd
     
 def getMatchIDs(offset):
-    
-    # Get matches information
-   
+    # This function goal is to get match IDs in each page on HLTV.org
     print('Get Match IDs from HLTV...')
     html = getHTML("https://www.hltv.org/results?offset={}".format(str(offset)))
 
@@ -28,7 +26,7 @@ def getMatchIDs(offset):
     return df
 
 def getTeamsInfo(soup, html):
-
+    # This function goal is to get teams info
     lineups = soup.find_all('div', attrs={'class':'lineups', 'id':'lineups'})
     lines = lineups[0].find_all('div', attrs={'class':'lineup standard-box'})
 
@@ -68,6 +66,7 @@ def getTeamsInfo(soup, html):
     return teams
 
 def getPlayersInfo(soup):
+    # This function goal is to get players info
     lineups = soup.find_all('div', attrs={'class':'lineups', 'id':'lineups'})
     lines = lineups[0].find_all('div', attrs={'class':'lineup standard-box'})
 
@@ -99,7 +98,7 @@ def getPlayersInfo(soup):
     return players
 
 def getMaps(soup, html):
-    
+    # This function goal is to get maps played in each match
     matchMaps = list() # list to add maps played 
     # Get map names
     map = re.findall('<div class=\"mapname\">.*</div>', html)
@@ -121,6 +120,7 @@ def getMaps(soup, html):
     return matchMaps
 
 def getLineups(soup, matchID):
+    # This function goal is to get team lineups (player for each team)
     lineups = soup.find_all('div', attrs={'class':'lineups', 'id':'lineups'})
     lines = lineups[0].find_all('div', attrs={'class':'lineup standard-box'})
 
@@ -147,7 +147,7 @@ def getLineups(soup, matchID):
     return Lineups
 
 def getMatchInfos(soup, html, matchID):
-    
+    # This function goal is to extract match info for each map in each match
     # Get maps info to query data from each map 
     maps = getMaps(soup, html)
 
@@ -207,3 +207,66 @@ def getMatchInfos(soup, html, matchID):
         matchInfo = pd.concat([matchInfo, lineups], axis=0, ignore_index=True)
 
     return matchInfo
+
+def getMatchOverview(soup, html):
+    # This function goal is to get the match overview for each map, like winner, final score, etc.
+    # Get teams to add to a DataFrame 
+    teams = getTeamsInfo(soup, html)
+    teams = [teams for teams in teams['ID']]
+
+    MatchOverview = pd.DataFrame()
+
+    matchOverview = soup.find_all('div', attrs={'class':'played'})
+    matchOverviews = concat_tags(matchOverview)
+
+    for i, match in enumerate(matchOverviews):
+        mapName = re.findall(r'map-name-holder"><img alt="([^"]+)" class="minimap"', str(match))
+        mapName = mapName[0]
+
+        # Get match result (win/lost) and pick. If team 1 win this will be set True, the same if team 1 pick the map
+        finalResult = re.findall(r'class="results-left.*\"', str(match))
+        team1Win = 'won' in finalResult[0]
+        team1Pick = 'pick' in finalResult[0]
+
+        # Get final score
+        finalScore = re.findall(r'<div class="results-team-score">(\d+)</div>', str(match))
+        
+        # Get half results
+        total_halfs = re.findall(r'\((.*?)\)', str(match))
+        halfs = total_halfs[0].split(';')
+
+        startSide = 'CT' if 'ct' in halfs[0].split(':')[0] else 'TR'
+
+        if len(total_halfs) > 1:
+            overtime = re.findall(r'<span>(\d+)</span>', str(total_halfs[1]))
+
+            team1_over = int(overtime[0])
+            team2_over = int(overtime[1])
+        else:
+            team1_over = 0
+            team2_over = 0
+
+        columns = ['MapName','Team1','Team2','Team1_Pick','Team1_Win','Team1_Final_Score','Team2_Final_Score','Halfs_Played','Team1_Start_Side']
+        result = [mapName,int(teams[0]),int(teams[1]),team1Pick,team1Win,int(finalScore[0]),int(finalScore[1]),len(halfs),startSide]
+
+        for i, half in enumerate(halfs):
+            results = re.findall(r'class="(ct|t)">(\d+)</span>', str(half))
+            result.append(int(results[0][1]))
+            result.append(int(results[1][1]))
+
+            columns.append(f'Team1_Half{i+1}_Score')
+            columns.append(f'Team2_Half{i+1}_Score')
+
+        result.append(team1_over)
+        result.append(team2_over)
+
+        columns.append('Team1_OT_Score')
+        columns.append('Team2_OT_Score')
+
+        # Create a PataFrame to merge with the final DataFrame
+        mapOverview = pd.DataFrame(result).transpose()
+        mapOverview.columns = columns
+
+        MatchOverview = pd.concat([MatchOverview, mapOverview], axis=0, ignore_index=True)
+    
+    return MatchOverview
